bfb944bb4a4d8cd788c4559f0db007fb6b28d979
marcvincent <marcantoine.vincent@geoaddress.me>
2012-10-05 09:28:31 -0400

    geoAd - Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoAdTest::Application.config.secret_token = '09c4c621b562e5ed58073ec78482f4c7fa519afd7b351d54c1fa673a390a9774a3806c6fdfe37acd8eb722ef2b2f683d4a8a348d1d86c865fa29edf7c054179c'
